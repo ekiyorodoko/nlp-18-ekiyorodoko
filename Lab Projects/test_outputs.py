3d7470c3caa722f96d5683ded14595dd428083d8
Ekiyor Odoko <ekiyorodoko@gmail.com>
2018-09-17 20:23:21 +0100

    test cases both ways

--- Lab Projects/test_outputs.py
+++ Lab Projects/test_outputs.py
@@ -8,10 +8,13 @@ from data_wrangler import extract_from_files
 def main():
     paths = ["dataset/yelp_labelled.txt"]
 
+    # Output size test result
     if (feature_test(paths)==(3000,2)):
-        print("Features passed output test")
+        print("Features passed output size test")
+    else:
+        print("Features failed output size test")
 
-#   Specific test for feature output from data_wrangler.py
+#   Specific test for feature output
 def feature_test(d):
     f = extract_from_files(d)
     shape = f.shape

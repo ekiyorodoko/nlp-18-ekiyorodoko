14b4c2ec0d704f05a772a88ae22acaccd5010036
Ekiyor Odoko <ekiyorodoko@gmail.com>
2018-11-11 22:13:28 +0000

    prediciton for unnormalized naive bayes

--- Lab Projects/LabProject4/lab4.py
+++ Lab Projects/LabProject4/lab4.py
@@ -2,26 +2,47 @@ import sys
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+
 
 def main():
     # Read from standard input
     input_ = sys.argv
-    classifier, version, output_file = input_[1], input_[2], input_[3]
+    classifier_type, version, output_file = input_[1], input_[2], input_[3]
     
-    if classifier == "nb":
+    if classifier_type == "nb":
+        classifier = GaussianNB()
         #  Load data using pandas dataframe
         data = pd.read_table("dataset.txt", sep='\t', header=None, names=['text', 'label'])
-        if version == "u":    
-            X_train, X_test, y_train, y_test = train_test_split(data['text'], data['label'], test_size=0.2, random_state=69)
-        else:
-            normalize()
+        if version == "n":    
+            normalize(data)
+        
+        # Split data into training and testing
+        X_train, X_test, y_train, y_test = train_test_split(data['text'], data['label'], test_size=0.2, random_state=69)
 
+        # Predict labels for test documents
+        y_pred = classifier.fit(X_train, y_train).predict(X_test)
+
+    else: 
+        if version = "n":
+            #
+        else:
+            #
 
 # Normalize document text
 def normalize(dataset):
-X_train, X_test, y_train, y_test = train_test_split(data['text'], data['label'], test_size=0.2, random_state=69)
+    # casefolding: convert all characters in document to lower case
+    dataset['text'] = dataset.text.map(lambda x: x.lower())
+
+    # remove all punctuation
+    dataset['text'] = dataset.text.replace('[^\w\s]', '')
+
+    # tokenization: convert text into single words
+
+
+    X_train, X_test, y_train, y_test = train_test_split(dataset['text'], dataset['label'], test_size=0.2, random_state=19)
 
 # count_vect = CountVectorizer()  
-# counts = count_vect.fit_transform(data['text'])  
+# counts = count_vect.fit_transform(data['text']) 
 
-print(len(y_train))
+

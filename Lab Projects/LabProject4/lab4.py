6f9aa51ed37eb1674af903400034cd6e00f7e08d
Ekiyor Odoko <ekiyorodoko@gmail.com>
2018-11-11 21:23:10 +0000

    completed code for unnormalized text withnaive bayes

--- Lab Projects/LabProject4/lab4.py
+++ Lab Projects/LabProject4/lab4.py
@@ -4,18 +4,21 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 
 def main():
-    #  Load data using pandas dataframe
-    data = pd.read_table("dataset.txt", sep='\t', header=None, names=['text', 'label'])
+    # Read from standard input
+    input_ = sys.argv
+    classifier, version, output_file = input_[1], input_[2], input_[3]
     
-    X_train, X_test, y_train, y_test = train_test_split(data['text'], data['label'], test_size=0.2, random_state=69)
+    if classifier == "nb":
+        #  Load data using pandas dataframe
+        data = pd.read_table("dataset.txt", sep='\t', header=None, names=['text', 'label'])
+        if version == "u":    
+            X_train, X_test, y_train, y_test = train_test_split(data['text'], data['label'], test_size=0.2, random_state=69)
+        else:
+            normalize()
 
 
 # Normalize document text
-def normalize(text, label):
-
-
-
-
+def normalize(dataset):
 X_train, X_test, y_train, y_test = train_test_split(data['text'], data['label'], test_size=0.2, random_state=69)
 
 # count_vect = CountVectorizer()  
